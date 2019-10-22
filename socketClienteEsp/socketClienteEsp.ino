c1bff7c2a5a1d3ffd1122c222df182f4dc8ff3b0
Bolsista <Bolsista>
2019-10-22 11:51:05 -0300

    tentativa de conexao com outros ips no esp32, e agora é perceptível ao esp se o servidor fechar

--- socketClienteEsp/socketClienteEsp.ino
+++ socketClienteEsp/socketClienteEsp.ino
@@ -7,8 +7,12 @@ const char* ssid = "ESP32-REDE";
 const char* password =  "123456789";
 IPAddress gateway;
 
+WiFiClient client;
+int number = 2;
+char* host ="";
+
 const uint16_t port = 9999;
-const char* host = "192.168.4.2";
+//const char* host = "192.168.4.2";//192.168.4.2
 
 void setup() {
  Serial.begin(9600);
@@ -24,14 +28,12 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-  Serial.println("ola");
-  WiFiClient client;
-
-  if(!client.connect(host,port)){
-    Serial.println("conexão falhada com o servidor");
-    delay(4000);
+  if(tentaConexao()== false){
+    number++;
+    if(number>5)
+      number = 2;
     return;
-  }
+  }else{
 
   Serial.println("conexão com o servidor concluida");
   Serial.println("vai enviar mensagem");
@@ -41,21 +43,48 @@ void loop() {
   //client.stop;
   Serial.println("esperando mensagem chegar");
   while(true){
+    if(!client.connected()){//verifica se o esp ainda está conectado
+      break;
+    }
     delay(200);
     if(client.available()){
       char mensagem = client.read();
       Serial.println("Mensagem recebida do servidor = ");
       Serial.println(mensagem);
       digitalWrite(led,HIGH);
-      //delay(500);
-      //client.flush();
-      //continue;  
     }else{
       //Serial.println("Sem dados a serem lidos");
       digitalWrite(led,LOW);
     }
   }
   Serial.println("conexão com o servidor fechada");
+  }
+  delay(1000);
   
-  delay(10000);
+}
+
+bool tentaConexao(){
+    mudaHost();
+    if(!client.connect(host,port)){
+    Serial.println("conexão falhada com o servidor");
+    Serial.println("______________________________");
+    //delay(4000);
+    return false;
+  }
+  return true;
+}
+
+void mudaHost(){
+  if(number == 2){
+    host = "192.168.4.2";
+  }else if(number == 3){
+    host = "192.168.4.3";
+  }else if(number == 4){
+    host = "192.168.4.4";
+  }else if(number == 5){
+    host = "192.168.4.5";
+  }
+  
+  Serial.print("tentando conexão com: ");
+  Serial.println(host);
 }
