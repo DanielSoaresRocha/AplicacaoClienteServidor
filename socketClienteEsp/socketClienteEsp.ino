0496c0874781de85ce199b18adbcf8cfbbaa372a
Daniel Soares <legoliga123@gmail.com>
2019-10-29 16:51:31 -0300

    adicionado servoMotor

--- socketClienteEsp/socketClienteEsp.ino
+++ socketClienteEsp/socketClienteEsp.ino
@@ -1,6 +1,8 @@
 #include <WiFi.h>
+#include <Servo.h>
 
-int led = 23;
+int ledVermelho = 22;
+int ledVerde = 23;
 
 //Bloco de configuração WIFI
 const char* ssid = "ESP32-REDE";
@@ -12,21 +14,29 @@ int number = 2;
 char* host ="";
 
 const uint16_t port = 9999;
-//const char* host = "192.168.4.2";//192.168.4.2
+
+//static const int servoPin = 2;
+Servo servo;
 
 void setup() {
- Serial.begin(9600);
-  pinMode(led, OUTPUT);
+  Serial.begin(115200);
+  servo.attach(4);
+  pinMode(ledVermelho, OUTPUT);
+  pinMode(ledVerde, OUTPUT);
   //criando rede
   Serial.println("Criando rede...");
   WiFi.softAP(ssid, password);
   IPAddress IP = WiFi.softAPIP();
   Serial.println("AP IP address: ");
   Serial.println(IP);
+  
+  
+  
 
 }
 
 void loop() {
+  digitalWrite(ledVermelho,HIGH);
   // put your main code here, to run repeatedly:
   if(tentaConexao()== false){
     number++;
@@ -34,7 +44,8 @@ void loop() {
       number = 2;
     return;
   }else{
-
+  //servo.write(0);
+  digitalWrite(ledVermelho,LOW);
   Serial.println("conexão com o servidor concluida");
   Serial.println("vai enviar mensagem");
   client.println("hello");
@@ -51,15 +62,31 @@ void loop() {
       char mensagem = client.read();
       Serial.println("Mensagem recebida do servidor = ");
       Serial.println(mensagem);
-      digitalWrite(led,HIGH);
+      moverMotor();
+      digitalWrite(ledVerde,HIGH);
     }else{
       //Serial.println("Sem dados a serem lidos");
-      digitalWrite(led,LOW);
+      digitalWrite(ledVerde,LOW);
     }
   }
   Serial.println("conexão com o servidor fechada");
   }
   delay(1000);
+  
+}
+
+void moverMotor(){
+  for(int posDegres = 0; posDegres <= 178; posDegres++){
+    servo.write(posDegres);
+    Serial.println(posDegres);
+    delay(20);
+  }
+  delay(3000);
+  for(int posDegres = 178; posDegres >= 0; posDegres--){
+    servo.write(posDegres);
+    Serial.println(posDegres);
+    delay(20);
+  }
   
 }
 
