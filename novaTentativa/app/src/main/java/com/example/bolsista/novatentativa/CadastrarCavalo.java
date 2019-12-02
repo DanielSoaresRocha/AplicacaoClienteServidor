060997cf250a98cf80fb651ee738ef1f819140e6
Daniel Soares <legoliga123@gmail.com>
2019-12-02 10:29:19 -0300

    adicionando cavalo no FireBase FireStore

--- novaTentativa/app/src/main/java/com/example/bolsista/novatentativa/CadastrarCavalo.java
+++ novaTentativa/app/src/main/java/com/example/bolsista/novatentativa/CadastrarCavalo.java
@@ -1,15 +1,24 @@
 package com.example.bolsista.novatentativa;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class CadastrarCavalo extends AppCompatActivity {
 
@@ -17,8 +26,13 @@ public class CadastrarCavalo extends AppCompatActivity {
     TextView nome, idade, raca, detalhes;
 
 
-    private FirebaseDatabase mFirebaseDatabase;
-    private DatabaseReference mDatabaseReference;
+    //private FirebaseDatabase mFirebaseDatabase;
+    //private DatabaseReference mDatabaseReference;
+
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+    Map<String, Object> user = new HashMap<>();
+    Map<String, Object> equino = new HashMap<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,9 +41,14 @@ public class CadastrarCavalo extends AppCompatActivity {
 
         inicializar();
 
-        mFirebaseDatabase = FirebaseDatabase.getInstance();
-        mDatabaseReference = mFirebaseDatabase.getReference().child("equinos-qi");
+        //mFirebaseDatabase = FirebaseDatabase.getInstance();
+        //mDatabaseReference = mFirebaseDatabase.getReference().child("equinos-qi");
 
+        user.put("first", "Ada");
+        user.put("last", "Lovelace");
+        user.put("born", 1815);
+
+        //addFireStore();
         listener();
 
 
@@ -39,10 +58,16 @@ public class CadastrarCavalo extends AppCompatActivity {
         cadastrarCavaloBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                equino.put("idade",Integer.parseInt(idade.getText().toString()));
+                equino.put("nome", nome.getText().toString());
+                equino.put("raca",raca.getText().toString());
+                equino.put("detalhes",detalhes.getText().toString());
+
+                addFireStore();
+                /*
                 mDatabaseReference.push().setValue(new Cavalo(nome.getText().toString(),
                         raca.getText().toString(), Integer.parseInt(idade.getText().toString()),
-                        detalhes.getText().toString()));
-
+                        detalhes.getText().toString()));*/
                 nome.setText("");
                 raca.setText("");
                 idade.setText("");
@@ -51,6 +76,24 @@ public class CadastrarCavalo extends AppCompatActivity {
             }
         });
     }
+
+    public void addFireStore(){
+        db.collection("equinos")
+                .add(equino)
+                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                    @Override
+                    public void onSuccess(DocumentReference documentReference) {
+                        Log.i("DataBase-FireStore", "DocumentSnapshot added with ID: " + documentReference.getId());
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.i("DataBase-FireStore", "Error adding document", e);
+                    }
+                });
+    }
+
 
     private void inicializar() {
         cadastrarCavaloBtn = findViewById(R.id.cadastrarCavaloBtn);
