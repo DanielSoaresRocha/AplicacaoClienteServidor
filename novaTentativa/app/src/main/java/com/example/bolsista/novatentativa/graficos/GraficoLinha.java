56e8588ab23f6ccbbf7183c0921db79672054654
DanielSoaresRocha <legoliga123@gmail.com>
2021-03-12 18:01:57 -0300

    Adicionando imagem do gráfico no PDF

--- novaTentativa/app/src/main/java/com/example/bolsista/novatentativa/graficos/GraficoLinha.java
+++ novaTentativa/app/src/main/java/com/example/bolsista/novatentativa/graficos/GraficoLinha.java
@@ -1,9 +1,15 @@
 package com.example.bolsista.novatentativa.graficos;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
 import android.os.Bundle;
-import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.example.bolsista.novatentativa.R;
 import com.example.bolsista.novatentativa.modelo.Sessao;
@@ -11,7 +17,15 @@ import com.github.mikephil.charting.charts.LineChart;
 import com.github.mikephil.charting.components.Legend;
 import com.github.mikephil.charting.components.XAxis;
 import com.github.mikephil.charting.components.YAxis;
+import com.itextpdf.io.image.ImageDataFactory;
+import com.itextpdf.io.source.ByteArrayOutputStream;
+import com.itextpdf.kernel.geom.PageSize;
+import com.itextpdf.kernel.pdf.PdfDocument;
+import com.itextpdf.kernel.pdf.PdfWriter;
+import com.itextpdf.layout.Document;
+import com.itextpdf.layout.element.Image;
 
+import java.io.File;
 import java.util.ArrayList;
 
 public class GraficoLinha extends AppCompatActivity {
@@ -19,6 +33,10 @@ public class GraficoLinha extends AppCompatActivity {
     Graficos g;
     ArrayList<Sessao> sessoes;
     String nomeEquino;
+    Button gerarGrafico;
+
+    private final int REQUEST_CODE = 3434;
+    public static final String DEST = "/sdcard/teste.pdf";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +48,7 @@ public class GraficoLinha extends AppCompatActivity {
         layoutGraficoLinha();
 
         g.getGraficoLinha(graficoLinha,sessoes, nomeEquino);
+        listener();
     }
 
     private void getSessoes() {
@@ -60,8 +79,51 @@ public class GraficoLinha extends AppCompatActivity {
         //graficoLinha.setVisibleXRangeMaximum(10f);// valor inicial mínimo - eixo 'x' - Visível
     }
 
+    private void listener(){
+        gerarGrafico.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try{
+                    File file = new File(DEST);
+                    if (!file.exists()) {
+                        file.createNewFile();
+                    }
+                    file.getParentFile().mkdirs();
+                    manipulatePdf(DEST);
+                }catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+    }
+
+    private void manipulatePdf(String dest) throws Exception {
+        PdfDocument pdfDoc = new PdfDocument(new PdfWriter(dest));
+        Document doc = new Document(pdfDoc, PageSize.A4.rotate());
+
+        graficoLinha.buildDrawingCache();
+        Bitmap bm = graficoLinha.getDrawingCache();
+
+        ByteArrayOutputStream stream3 = new ByteArrayOutputStream();
+        bm.compress(Bitmap.CompressFormat.PNG, 100, stream3);
+        byte[] byteArray = stream3.toByteArray();
+
+        Image gray = new Image(ImageDataFactory.create(byteArray));
+        gray.scaleToFit(1000, 500);
+
+        doc.add(gray);
+
+        doc.close();
+        Toast.makeText(this, DEST+"is saved to", Toast.LENGTH_SHORT).show();
+    }
+
     private void inicializar() {
         g = new Graficos();
         graficoLinha = (LineChart) findViewById(R.id.graficoLinha);
+        gerarGrafico = findViewById(R.id.gerarGrafico);
+
+        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, REQUEST_CODE);
+
     }
 }
